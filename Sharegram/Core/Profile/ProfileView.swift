7ddb9635df0e3632e1605f8871022da266b195d7
Lucas Jorge Hubert <lucasjhubert@gmail.com>
2023-05-27 00:30:56 -0300

    Profile Navigation created

--- Sharegram/Core/Profile/ProfileView.swift
+++ Sharegram/Core/Profile/ProfileView.swift
@@ -8,66 +8,95 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
     var body: some View {
-        VStack {
-            // Header
-            VStack(spacing: 10) {
-                // Pic and stats
-                HStack {
-                    Image("userImage")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
-                    
-                    Spacer()
-                    
-                    HStack(spacing: 8) {
+        NavigationStack {
+            ScrollView {
+                // Header
+                VStack(spacing: 10) {
+                    // Pic and stats
+                    HStack {
+                        Image("userImage")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
                         
-                        UserStatView(value: 3, title: "Posts")
-                        UserStatView(value: 3, title: "Followers")
-                        UserStatView(value: 3, title: "Following")
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStatView(value: 3, title: "Posts")
+                            UserStatView(value: 3, title: "Followers")
+                            UserStatView(value: 3, title: "Following")
+                        }
+                    }
+                    .padding(.horizontal)
+                    .padding(.bottom, 4)
+                    
+                    // Name and bio
+                    VStack(alignment: .leading) {
+                        Text("lkshubert")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Text("Lucas Hubert")
+                            .font(.footnote)
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    // actionButton
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundColor(.purple)
+                            .overlay(RoundedRectangle(cornerRadius: 6)
+                                .stroke(
+                                    Color.purple,
+                                    lineWidth: 1
+                                )
+                            )
+                    }
+
+                    
+                    Divider()
+                }
+                .padding(.top, 8)
+                
+                // postGrid
+                
+                LazyVGrid(columns: gridItems, spacing: 1) {
+                    ForEach(0 ... 15, id: \.self) { index in
+                        Image("userImage")
+                            .resizable()
+                            .scaledToFill()
                     }
                 }
-                .padding(.horizontal)
-                .padding(.bottom, 4)
-                
-                // Name and bio
-                VStack(alignment: .leading) {
-                    Text("lkshubert")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                    
-                    Text("Lucas Hubert")
-                        .font(.footnote)
-                    
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                
-                // actionButton
-                
-                Button {
-                    
-                } label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .frame(width: 360, height: 32)
-                        .foregroundColor(.purple)
-                        .overlay(RoundedRectangle(cornerRadius: 6)
-                            .stroke(
-                                Color.purple,
-                                lineWidth: 1
-                            )
-                        )
-                }
-
-                
-                Divider()
             }
-            
-            // postGrid
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.automatic)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundColor(.black)
+                    }
+
+                }
+            }
         }
     }
 }
