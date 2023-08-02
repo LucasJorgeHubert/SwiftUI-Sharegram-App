f9f0e27901b2abdc4ddb045a0bc7e427626640c1
Lucas Jorge Hubert <lucasjhubert@gmail.com>
2023-08-02 20:12:00 -0300

    upload profile image works

--- Sharegram/Core/Profile/ViewModel/EditProfileViewModel.swift
+++ Sharegram/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -21,6 +21,8 @@ class EditProfileViewModel: ObservableObject {
     @Published var fullname = ""
     @Published var bio = ""
     
+    private var uiImage: UIImage?
+    
     init(user: User) {
         self.user = user
     }
@@ -30,12 +32,18 @@ class EditProfileViewModel: ObservableObject {
         
         guard let data = try? await item.loadTransferable(type: Data.self) else { return }
         guard let uiImage = UIImage(data: data) else { return }
+        self.uiImage = uiImage
         self.profileImage = Image(uiImage: uiImage)
     }
     
     func updateUserData() async throws {
         
         var data = [String:Any]()
+        
+        if let uiImage = uiImage {
+            let imageURL = try? await ImageUploader.uploadImage(image: uiImage)
+            data["profileImageURL"] = imageURL
+        }
         
         if !fullname.isEmpty && user.fullname != fullname {
             data["fullname"] = fullname
